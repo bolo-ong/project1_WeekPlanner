122c92b8c4bef1f9a247fa509587ba21f6480f33
Lim Chaemin <wldnjscoals@gmail.com>
2023-01-18 04:56:05 +0900

    express rotuer

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-const mongoose = require('./schemas');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('./schemas');
 const port = process.env.PORT;
+
+const routineRouter = require('./routes/routine')
 
 
 app.use(cors());
@@ -12,5 +14,6 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+app.use('/routine', routineRouter)
 
-
+console.log(process.env.PORT)
