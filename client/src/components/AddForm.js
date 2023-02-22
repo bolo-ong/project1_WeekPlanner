ecf01caa4073e147fb2f66788a2eced9b9d08646
Lim Chaemin <wldnjscoals@gmail.com>
2023-02-22 23:14:20 +0900

    install & using 'formik' & 'yup'

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,5 +1,6 @@
 import { useRef, useEffect, useState } from "react";
-
+import { useFormik } from "formik"
+import * as yup from "yup"
 
 function AddForm({ setAddForm, setRoutines, routines }) {
     const inputRef = useRef();
@@ -22,6 +23,27 @@ function AddForm({ setAddForm, setRoutines, routines }) {
             document.removeEventListener('keydown', handleKeyDown);
         };
     }, []);
+
+
+    const [weekCheck, setWeekCheck] = useState({
+        "월": false,
+        "화": false,
+        "수": false,
+        "목": false,
+        "금": false,
+        "토": false,
+        "일": false
+    });
+    const week = Object.entries(weekCheck)
+
+    const handleWeekCheck = (e) => {
+        const copy = { ...weekCheck };
+        copy[e.target.id] = e.target.checked;
+        setButtonUiController(Object.values(copy).includes(true))
+        setWeekCheck(copy)
+    }
+
+    const [buttonUiController, setButtonUiController] = useState(false)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -52,29 +74,38 @@ function AddForm({ setAddForm, setRoutines, routines }) {
         }
     }
 
+    const validationSchema = yup.object().shape({
+        title: yup
+            .string()
+            .required('제목을 입력해 주세요.')
+            .max(100, '100자 이내로 작성해 주세요.'),
+        time: yup
+            .string()
+            .required('시간을 선택해 주세요.'),
+        description: yup
+            .string()
+            .max(300, '300자 이내로 작성해 주세요.'),
+        checked: yup
+            .array()
+            .required('요일을 선택해 주세요.')
+    })
 
-    const [weekCheck, setWeekCheck] = useState({
-        "월": false,
-        "화": false,
-        "수": false,
-        "목": false,
-        "금": false,
-        "토": false,
-        "일": false
-    });
-    const week = Object.keys(weekCheck)
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            time: '',
+            description: '',
+            checked: []
+        },
+        onSubmit: values => {
+            console.log(values)
+        },
+        validationSchema
+    })
 
-    const handleWeekCheck = (e) => {
-        const copy = { ...weekCheck };
-        copy[e.target.id] = e.target.checked;
-        setButtonUiController(Object.values(copy).includes(true))
-        setWeekCheck(copy)
-    }
-
-    const [buttonUiController, setButtonUiController] = useState(false)
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
             <div className="fixed w-full h-full z-10">
                 <div className="w-full h-full bg-black opacity-80"
                     onClick={() => {
@@ -91,36 +122,52 @@ function AddForm({ setAddForm, setRoutines, routines }) {
                             <div className="flex justify-between">
                                 <input className=" mt-1 p-1 mr-1 w-min bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md focus:ring-1"
                                     ref={inputRef}
-                                    minLength="1"
+
                                     maxLength="100"
                                     placeholder="Title"
                                     type="text"
                                     name="title"
                                     required
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                                {formik.touched.title && formik.errors.title && <p>{formik.errors.title}</p>}
                                 <input className="ml-1 mt-1 p-1 w-32 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md focus:ring-1"
                                     type="time"
                                     name="time"
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
                                 />
+                                {formik.touched.time && formik.errors.time && <p>{formik.errors.time}</p>}
                             </div>
                         </div>
                         <div className=" text-gray-600">
                             <textarea className="mt-1 p-1 mr-1 h-32 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md focus:ring-1"
-                                minLength="1"
+
                                 maxLength="300"
                                 placeholder="Content"
                                 type="text"
                                 name="description"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
+                            {formik.touched.description && formik.errors.description && <p>{formik.errors.description}</p>}
                         </div>
                         <div className="flex justify-evenly mt-2">
                             {week.map((data, i) =>
                                 <div key={i}>
                                     <input
-                                        type='checkbox'
-                                        id={data}
-                                        onChange={handleWeekCheck}
+                                        type="checkbox"
+                                        name="checked"
+                                        id={data[0]}
+                                        checked={data[1]}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                    // checked={formik.values.checked.includes(data)}
+
+                                    // onChange={handleWeekCheck}
                                     />
+                                    {formik.touched.checked && formik.errors.checked && <p>{formik.errors.title}</p>}
                                     <label htmlFor={data}>{data}</label>
                                 </div>
                             )}
